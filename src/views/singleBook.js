efd0ac694e6d3b7a99c1632ec58f0977e1a5af9d
Jenny Gao <jennygao@Jennys-MBP.home>
2019-01-15 12:45:40 -0500

    Activate Single Book View

--- src/views/singleBook.js
+++ src/views/singleBook.js
@@ -1,48 +1,48 @@
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { withRouter } from 'react-router-dom';
-// import { fetchSingleBook } from '../store';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchSingleBook } from '../store';
 
-// class SingleBook extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//     }
-//   }
+class SingleBook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
 
-//   componentDidMount() {
-//     this.props.fetchSingleBook()
-//   }
+  componentDidMount() {
+    this.props.fetchSingleBook()
+  }
 
-//   render () {
-//     const { singleBook } = this.props;
-//     return (
-//       <div>
-//         <h3>Books</h3>
-//         {allBooks.map(book => {
-//           return (
-//             <div>{book.name}</div>
-//           )
-//         })}
-//         <BooksTable allBooksData={allBooks} singleBookData={singleBook}/>
-//       </div>
-//     )
-//   }
-// }
+  render () {
+    const { singleBook } = this.props;
+    return (
+      <div>
+        <h3>Books</h3>
+        {allBooks.map(book => {
+          return (
+            <div>{book.name}</div>
+          )
+        })}
+        <BooksTable allBooksData={allBooks} singleBookData={singleBook}/>
+      </div>
+    )
+  }
+}
 
-// const mapStateToProps = state => {
-//     const singleBook = state.singleBook
-//     return({
-//       singleBook,
-//     })
-//   };
+const mapStateToProps = state => {
+    const singleBook = state.singleBook
+    return({
+      singleBook,
+    })
+  };
   
-//   const mapDispatchToProps = dispatch => { 
-//     return({
-//       fetchSingleBook: () => { 
-//         dispatch(fetchSingleBook()) 
-//       }
-//     });
-//   };
+  const mapDispatchToProps = dispatch => { 
+    return({
+      fetchSingleBook: () => { 
+        dispatch(fetchSingleBook()) 
+      }
+    });
+  };
   
-//   export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SingleBook));
+  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SingleBook));
